9842b7099bfc78979ecf92e658664a4b0d8d6730
Anthony Finch <t@anthonyfinch.com>
2022-10-01 14:37:17 +0100

    adds some nuance to different enemy states, including waiting at investigation sites

--- enemy/Enemy.gd
+++ enemy/Enemy.gd
@@ -4,21 +4,24 @@ enum EnemyState {
 	Scanning,
 	Wandering,
 	Investigating,
+	Waiting,
 	Attacking
 }
 
-export (float) var turn_speed := 1.0
+export (float) var turn_speed := 0.4
 export (EnemyState) var state = EnemyState.Scanning
-export (float) var scan_time := 4.0
-export (float) var wander_speed := 2.0
+export (float) var scan_time := 6.0
+export (float) var wait_time := 4.0
+export (float) var investigate_time := 10.0
+export (float) var wander_speed := 1.0
+export (float) var investigate_speed := 2.0
 export (float) var attack_speed := 5.0
-export (float) var total_visibility_cutoff := 0.75
+export (float) var total_visibility_cutoff := 0.65
 export (float) var partial_visibility_cutoff := 0.15
 
 onready var nav_agent : NavigationAgent = $NavigationAgent
 onready var timer : Timer = $Timer
 onready var vision_cone : Area = $VisionCone
-onready var vision_ray : RayCast = $VisionCone/RayCast
 onready var rad := PI / 180
 onready var patrol_points = get_tree().get_nodes_in_group("patrol_points")
 onready var label = $Label
@@ -29,7 +32,7 @@ var target := Vector3.ZERO
 func _ready():
 	randomize()
 	timer.connect("timeout", self, "_on_timeout")
-	_start_scanning()
+	_start_scanning(scan_time)
 
 func _set_path():
 	var map_rid = nav_agent.get_navigation_map()
@@ -43,8 +46,10 @@ func _on_timeout():
 			target = patrol_points[idx].global_transform.origin
 			_set_path()
 			state = EnemyState.Wandering
+		EnemyState.Waiting:
+			_start_scanning(investigate_time)
 
-func _process(delta):
+func _process(_delta):
 	var debug_text = ""
 	match state:
 		EnemyState.Attacking:
@@ -55,26 +60,22 @@ func _process(delta):
 			debug_text = "Scanning"
 		EnemyState.Wandering:
 			debug_text = "Wandering"
+		EnemyState.Waiting:
+			debug_text = "Waiting"
 	label.text = debug_text
 
-func _physics_process(_delta):
+func _check_for_player():
 	var bodies = vision_cone.get_overlapping_bodies()
 	if bodies.size() > 0:
-		# print("Maybe body")
 		var possible_body = bodies[0]
 		if possible_body is Player:
-			# print("Got player")
-			# vision_ray.set_cast_to(possible_body.global_transform.origin - vision_ray.global_transform.origin)
-			# vision_ray.force_raycast_update()
 			var space_state = get_world().direct_space_state
 			var from = global_transform.origin
 			var to = possible_body.global_transform.origin
 			var result = space_state.intersect_ray(from, to, [self])
 			if result:
-			# var body = vision_ray.get_collider()
 				var body = result['collider']
 				if body is Player:
-					print(body.visibility)
 					if body.visibility > total_visibility_cutoff:
 						target = body.global_transform.origin
 						_set_path()
@@ -87,28 +88,44 @@ func _physics_process(_delta):
 						print("Who goes there?")
 					else:
 						print("Huh, nothing to see")
+
+func _physics_process(_delta):
+	_check_for_player()
 	match state:
 		EnemyState.Scanning:
 			global_rotate(Vector3.UP, rad * turn_speed)
 		EnemyState.Wandering:
-			_navigate_to_point(wander_speed)
+			var done = _navigate_to_point(wander_speed)
+			if done:
+				_start_scanning(scan_time)
 		EnemyState.Investigating:
-			_navigate_to_point(wander_speed)
+			var done = _navigate_to_point(investigate_speed)
+			if done:
+				_start_waiting()
 		EnemyState.Attacking:
-			_navigate_to_point(attack_speed)
+			var done = _navigate_to_point(attack_speed)
+			if done:
+				_start_scanning(investigate_time)
 
-func _start_scanning():
+func _start_waiting():
+	state = EnemyState.Waiting
+	timer.wait_time = wait_time
+	timer.start()
+
+func _start_scanning(time):
 	state = EnemyState.Scanning
-	timer.wait_time = scan_time
+	timer.wait_time = time
 	timer.start()
 
 func _navigate_to_point(speed):
 	var next_move = nav_agent.get_next_location()
 	var dir = (next_move - get_global_transform().origin).normalized()
 
+	# TODO replace this with a lerp to turn gradually
 	look_at(next_move, Vector3.UP)
 
 	if not nav_agent.is_navigation_finished():
 		move_and_slide(dir * speed)
+		return false
 	else:
-		_start_scanning()
+		return true
